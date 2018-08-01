92b245f90cffde86897f8940ce6d5b2e12b3ad88
iqbal <iqbalshorfana@gmail.com>
2018-08-01 13:32:01 +0700

    tentang pembuatan

--- src/tubes/frame/frame_utama_if.java
+++ src/tubes/frame/frame_utama_if.java
@@ -28,20 +28,23 @@ public class frame_utama_if extends javax.swing.JFrame {
 
         panel1 = new javax.swing.JPanel();
         jToolBar1 = new javax.swing.JToolBar();
-        jLabel2 = new javax.swing.JLabel();
+        j_mahasiswa_if = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JToolBar.Separator();
-        jLabel1 = new javax.swing.JLabel();
+        j_data_matakuliah_if = new javax.swing.JLabel();
         jSeparator2 = new javax.swing.JToolBar.Separator();
-        jLabel3 = new javax.swing.JLabel();
+        j_data_nilai_if = new javax.swing.JLabel();
         jSeparator3 = new javax.swing.JToolBar.Separator();
-        jLabel4 = new javax.swing.JLabel();
+        j_simulasi_nilai_akhir_if = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
+        jLabel7 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        jPanel3 = new javax.swing.JPanel();
         jLabel6 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        menu_tentang_pembuat_if = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -51,113 +54,126 @@ public class frame_utama_if extends javax.swing.JFrame {
 
         jToolBar1.setRollover(true);
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tubes/gambar/mahasiswa.png"))); // NOI18N
-        jLabel2.setText("Data Mahsiswa");
-        jLabel2.addMouseListener(new java.awt.event.MouseAdapter() {
+        j_mahasiswa_if.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tubes/gambar/mahasiswa.png"))); // NOI18N
+        j_mahasiswa_if.setText("Data Mahsiswa");
+        j_mahasiswa_if.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jLabel2MouseClicked(evt);
+                j_mahasiswa_ifMouseClicked(evt);
             }
         });
-        jToolBar1.add(jLabel2);
+        jToolBar1.add(j_mahasiswa_if);
         jToolBar1.add(jSeparator1);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tubes/gambar/clipboard.png"))); // NOI18N
-        jLabel1.setText("Data Mata Kuliah");
-        jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+        j_data_matakuliah_if.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tubes/gambar/clipboard.png"))); // NOI18N
+        j_data_matakuliah_if.setText("Data Mata Kuliah");
+        j_data_matakuliah_if.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jLabel1MouseClicked(evt);
+                j_data_matakuliah_ifMouseClicked(evt);
             }
         });
-        jToolBar1.add(jLabel1);
+        jToolBar1.add(j_data_matakuliah_if);
         jToolBar1.add(jSeparator2);
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tubes/gambar/1486164728-118_79708.png"))); // NOI18N
-        jLabel3.setText("Data Nilai");
-        jLabel3.addMouseListener(new java.awt.event.MouseAdapter() {
+        j_data_nilai_if.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tubes/gambar/1486164728-118_79708.png"))); // NOI18N
+        j_data_nilai_if.setText("Data Nilai");
+        j_data_nilai_if.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jLabel3MouseClicked(evt);
+                j_data_nilai_ifMouseClicked(evt);
             }
         });
-        jToolBar1.add(jLabel3);
+        jToolBar1.add(j_data_nilai_if);
         jToolBar1.add(jSeparator3);
 
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tubes/gambar/zoom_search_icon-icons.com_75907.png"))); // NOI18N
-        jLabel4.setText("Simulasi Nilai Akhir");
-        jLabel4.addMouseListener(new java.awt.event.MouseAdapter() {
+        j_simulasi_nilai_akhir_if.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tubes/gambar/zoom_search_icon-icons.com_75907.png"))); // NOI18N
+        j_simulasi_nilai_akhir_if.setText("Simulasi Nilai Akhir");
+        j_simulasi_nilai_akhir_if.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jLabel4MouseClicked(evt);
+                j_simulasi_nilai_akhir_ifMouseClicked(evt);
             }
         });
-        jToolBar1.add(jLabel4);
+        jToolBar1.add(j_simulasi_nilai_akhir_if);
 
         panel1.add(jToolBar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 650, 80));
 
         jPanel1.setBackground(new java.awt.Color(204, 204, 204));
 
-        jLabel5.setText("Poto 1");
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tubes/gambar/faldi.jpg"))); // NOI18N
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(100, Short.MAX_VALUE)
-                .addComponent(jLabel5)
-                .addGap(96, 96, 96))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(115, 115, 115)
-                .addComponent(jLabel5)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tubes/gambar/iqbal.jpg"))); // NOI18N
 
-        jLabel6.setText("Poto 2");
+        jLabel6.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jLabel6.setText("10116493");
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addContainerGap(100, Short.MAX_VALUE)
-                .addComponent(jLabel6)
-                .addGap(96, 96, 96))
-        );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGap(118, 118, 118)
-                .addComponent(jLabel6)
-                .addContainerGap(129, Short.MAX_VALUE))
-        );
+        jLabel8.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jLabel8.setText("10116493");
+
+        jLabel9.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jLabel9.setText("Muhammad Iqbal Shorfana");
+
+        jLabel10.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jLabel10.setText("Faldi Favian");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(47, 47, 47)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(88, 88, 88)
-                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(61, Short.MAX_VALUE))
+                .addGap(19, 19, 19)
+                .addComponent(jLabel7)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
+                .addComponent(jLabel5)
+                .addGap(20, 20, 20))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(135, 135, 135)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel10))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel8)
+                        .addGap(134, 134, 134))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel9)
+                        .addGap(89, 89, 89))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(33, 33, 33)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(36, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel7))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel8))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel9)
+                    .addComponent(jLabel10))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        panel1.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 80, 650, 330));
+        panel1.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 80, 650, 390));
 
         getContentPane().add(panel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         jMenu1.setText("Menu");
+
+        menu_tentang_pembuat_if.setText("Tentang Pembuat");
+        menu_tentang_pembuat_if.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                menu_tentang_pembuat_ifMouseClicked(evt);
+            }
+        });
+        menu_tentang_pembuat_if.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menu_tentang_pembuat_ifActionPerformed(evt);
+            }
+        });
+        jMenu1.add(menu_tentang_pembuat_if);
+
         jMenuBar1.add(jMenu1);
 
         setJMenuBar(jMenuBar1);
@@ -166,37 +182,40 @@ public class frame_utama_if extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseClicked
-        // TODO add your handling code here:
+    private void j_mahasiswa_ifMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_j_mahasiswa_ifMouseClicked
         frame_mahasiswa_if mhs_if = new frame_mahasiswa_if();
         mhs_if.setVisible(true);
-        
         this.setVisible(false);
-    }//GEN-LAST:event_jLabel2MouseClicked
+    }//GEN-LAST:event_j_mahasiswa_ifMouseClicked
 
-    private void jLabel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseClicked
-        // TODO add your handling code here:
+    private void j_data_matakuliah_ifMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_j_data_matakuliah_ifMouseClicked
         frame_matkul_if mhs_if = new frame_matkul_if();
         mhs_if.setVisible(true);
-        
         this.setVisible(false);
-    }//GEN-LAST:event_jLabel1MouseClicked
+    }//GEN-LAST:event_j_data_matakuliah_ifMouseClicked
 
-    private void jLabel3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel3MouseClicked
-        // TODO add your handling code here:
+    private void j_data_nilai_ifMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_j_data_nilai_ifMouseClicked
         frame_nilai_if_1 mhs_if = new frame_nilai_if_1();
         mhs_if.setVisible(true);
-        
         this.setVisible(false);
-    }//GEN-LAST:event_jLabel3MouseClicked
+    }//GEN-LAST:event_j_data_nilai_ifMouseClicked
 
-    private void jLabel4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel4MouseClicked
-        // TODO add your handling code here:
+    private void j_simulasi_nilai_akhir_ifMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_j_simulasi_nilai_akhir_ifMouseClicked
         frame_simulasiNilaiAkhir_if mhs_if = new frame_simulasiNilaiAkhir_if();
         mhs_if.setVisible(true);
-        
         this.setVisible(false);
-    }//GEN-LAST:event_jLabel4MouseClicked
+    }//GEN-LAST:event_j_simulasi_nilai_akhir_ifMouseClicked
+
+    private void menu_tentang_pembuat_ifActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menu_tentang_pembuat_ifActionPerformed
+        frame_tentang_pembuat_if tentang = new frame_tentang_pembuat_if();
+        tentang.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_menu_tentang_pembuat_ifActionPerformed
+
+    private void menu_tentang_pembuat_ifMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menu_tentang_pembuat_ifMouseClicked
+        // TODO add your handling code here:
+
+    }//GEN-LAST:event_menu_tentang_pembuat_ifMouseClicked
 
     /**
      * @param args the command line arguments
@@ -234,21 +253,24 @@ public class frame_utama_if extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
     private javax.swing.JToolBar.Separator jSeparator1;
     private javax.swing.JToolBar.Separator jSeparator2;
     private javax.swing.JToolBar.Separator jSeparator3;
     private javax.swing.JToolBar jToolBar1;
+    private javax.swing.JLabel j_data_matakuliah_if;
+    private javax.swing.JLabel j_data_nilai_if;
+    private javax.swing.JLabel j_mahasiswa_if;
+    private javax.swing.JLabel j_simulasi_nilai_akhir_if;
+    private javax.swing.JMenuItem menu_tentang_pembuat_if;
     private javax.swing.JPanel panel1;
     // End of variables declaration//GEN-END:variables
 }
